f471efc1dd0355b62be544e9fef5fdfcdf883099
THISHUKUMAR <thishukumarab77@gmail.com>
2025-12-05 11:11:29 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -5,10 +5,14 @@ import pandas as pd
 from PIL import Image
 from pdf2image import convert_from_path
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 from langchain_community.vectorstores import FAISS
 from langchain.schema import Document
-from langchain.embeddings import HuggingFaceEmbeddings
+# from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
 
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 
@@ -216,4 +220,5 @@ if __name__ == "__main__":
     print("\nUser Question:", user_q)
     print("\nAnswer:")
     print(answer_query(vector_db, user_q))
+
 
